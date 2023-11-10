519831e107bda4d47dbdc68e5ff4c59bb124c2b3
Boris Besemer <borisbesemer@gmail.com>
2023-11-10 14:18:02 +0100

    chore: clean up code

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
 import { DocumentTypeDecoration } from "@graphql-typed-document-node/core";
+import invariant from "tiny-invariant";
 
 export const extractOperationName = (query: string) => {
 	const matches = query.match(/(query|mutation)\s(\w+)/);
@@ -47,12 +48,59 @@ export class TypedDocumentString<TResult, TVariables>
 	}
 }
 
+/**
+ * Prefix for error messages
+ */
 export const createErrorPrefix = (...args: any[]) =>
 	["Error in query:", ...args].join("\n");
 
+/**
+ * Simple wrapper to create a SHA256 hash with subtle crypto
+ * @param message Message to hash
+ */
 export const createSha256 = async (message: string) => {
 	const encoder = new TextEncoder();
 	const data = encoder.encode(message);
 
 	return await crypto.subtle.digest("SHA-256", data);
+};
+
+/**
+ * Helper function that parses the response body and returns the data or throws an error
+ * @param response Fetch response object
+ * @returns GraphQL response body
+ */
+export const handleResponse = async (response: Response) => {
+	invariant(
+		response.ok,
+		`Response not ok: ${response.status} ${response.statusText}`
+	);
+
+	const body = await response
+		.json()
+		.catch((err) => invariant(false, "Could not parse JSON from response"));
+
+	// Check for GraphQL errors
+	const hasErrors = body.errors?.length && body.errors.length > 0;
+	invariant(hasErrors, JSON.stringify(body.errors, null, 2));
+
+	return body;
+};
+
+/**
+ * Check if the response has a PersistedQueryNotFound error.
+ * @param response Fetch response object
+ */
+export const hasPersistedQueryError = async <T>(response: Response) => {
+	if (!response.ok) return false;
+
+	try {
+		const body = (await response.json()) as GqlResponse<T>;
+
+		return Boolean(
+			body?.errors?.find((item) => item.message === "PersistedQueryNotFound")
+		);
+	} catch (err) {
+		return false;
+	}
 };
