b59f4488657e392287e6bbf7ea905390f2ceac18
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-11-26 21:39:47 +0100

    Export the ClientFetcher type

---
+++ .changeset/spicy-buckets-mate.md
@@ -0,0 +1,5 @@
+---
+"@labdigital/graphql-fetcher": patch
+---
+
+Export the ClientFetcher type
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { initClientFetcher } from "./client";
+export type { ClientFetcher } from "./client";
 export { ClientGqlFetcherProvider, useClientGqlFetcher } from "./provider";
 export type { GraphQLError, GqlResponse } from "./helpers";
