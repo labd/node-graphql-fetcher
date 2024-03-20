ccef1cf58338142b5ca2b1fb08835e15413524d4
Boris Besemer <borisbesemer@gmail.com>
2024-03-20 10:22:38 +0100

    test(client): add test cases for persisted queries

--- src/client.test.ts
+++ src/client.test.ts
@@ -35,6 +35,9 @@ describe("gqlClientFetch", () => {
 	beforeEach(() => fetchMock.resetMocks());
 
 	const fetcher = initClientFetcher("https://localhost/graphql");
+	const persistedFetcher = initClientFetcher("https://localhost/graphql", {
+		persistedQueries: true,
+	});
 
 	it("should perform a query", async () => {
 		const mockedFetch = fetchMock.mockResponse(responseString);
@@ -61,6 +64,28 @@ describe("gqlClientFetch", () => {
 		);
 	});
 
+	it("should perform a persisted query when enabled", async () => {
+		const mockedFetch = fetchMock.mockResponse(responseString);
+
+		const gqlResponse = await persistedFetcher(query, {
+			myVar: "baz",
+		});
+
+		expect(gqlResponse).toEqual(response);
+		expect(mockedFetch).toHaveBeenCalledWith(
+			// When persisted queries are enabled, we suffix all the variables and extensions as search parameters
+			"https://localhost/graphql?op=myQuery&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22e5276e0694f661ef818210402d06d249625ef169a1c2b60383acb2c42d45f7ae%22%7D%7D&variables=%7B%22myVar%22%3A%22baz%22%7D",
+			{
+				// Query is persisted, uses GET to be cached by CDN
+				method: "GET",
+				// These exact headers should be set:
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	});
 	it("should perform a mutation", async () => {
 		const mockedFetch = fetchMock.mockResponse(responseString);
 		const gqlResponse = await fetcher(mutation, {
@@ -73,5 +98,34 @@ describe("gqlClientFetch", () => {
 			"https://localhost/graphql?op=myMutation",
 			expect.anything() // <- body, method, headers, etc, are tested in the above
 		);
+	});
+
+	it("should throw when fetch fails", () => {
+		fetchMock.mockReject(new Error("Network error"));
+
+		expect(fetcher(query, { myVar: "baz" })).rejects.toThrow("Network error");
+	});
+	it("should fallback to POST when persisted query is not found on the server", async () => {
+		const mockedFetch = fetchMock.mockResponses(
+			JSON.stringify({
+				errors: [{ message: "PersistedQueryNotFound" }],
+			}),
+			responseString
+		);
+
+		const gqlResponse = await persistedFetcher(query, {
+			myVar: "baz",
+		});
+
+		expect(gqlResponse).toEqual(response);
+		// Should do two calls, a GET and a POST request
+		expect(mockedFetch).toHaveBeenCalledTimes(2);
+	});
+	it("should throw when JSON can't be parsed", () => {
+		fetchMock.mockResponse("<p>Not JSON</p>");
+
+		const gqlResponse = fetcher(query, { myVar: "baz" });
+
+		expect(gqlResponse).rejects.toThrow();
 	});
 });
