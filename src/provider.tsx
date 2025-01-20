1e499f4a8992d4a246b69408aff360613694250c
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-01-20 21:00:34 +0100

    style: import react type directly

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,7 +1,7 @@
 import type { ClientFetcher } from "client";
 import { createContext, useContext } from "react";
 import invariant from "tiny-invariant";
-import type * as React from "react";
+import type { PropsWithChildren } from "react";
 
 /**
  * Context to provide the fetcher for the API used during client side calls
@@ -10,7 +10,7 @@ export const ClientGqlFetcherContext = createContext<ClientFetcher | undefined>(
 	undefined,
 );
 
-export type ClientGqlFetcherProviderProps = React.PropsWithChildren<{
+export type ClientGqlFetcherProviderProps = PropsWithChildren<{
 	fetcher: ClientFetcher;
 }>;
 
